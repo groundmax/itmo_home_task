1c8583621f208d580a767c3cd003cbd3f4c4e71b
Фельдман Эмилий Олегович <eofeldman@0000nba0a4zmd6m.msk.mts.ru>
2022-10-13 14:35:35 +0500

    extended init file

--- requestor/db/__init__.py
+++ requestor/db/__init__.py
@@ -1,9 +1,12 @@
-from .exceptions import DuplicatedModelError, DuplicatedTeamError, TeamNotFoundError
+from .exceptions import DuplicatedModelError, DuplicatedTeamError, DuplicatedMetricError, TeamNotFoundError, ModelNotFoundError, TrialNotFoundError
 from .service import DBService
 
 __all__ = (
     "DuplicatedTeamError",
-    "TeamNotFoundError",
     "DuplicatedModelError",
+    "DuplicatedMetricError",
+    "TeamNotFoundError",
+    "ModelNotFoundError",
+    "TrialNotFoundError",
     "DBService",
 )
