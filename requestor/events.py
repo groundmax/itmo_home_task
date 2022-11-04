ef59ec892e70ff838e4e30406e65661d0a7a016f
Фельдман Эмилий Олегович <eofeldman@0000nba0a4zmd6m.msk.mts.ru>
2022-11-04 19:34:28 +0500

    fixed events

--- requestor/events.py
+++ requestor/events.py
@@ -1,19 +1,22 @@
 from aiogram import Bot, Dispatcher
 import typing as tp
 
+from requestor.services import App
 
 EventHandler = tp.Callable[[Dispatcher], None]
 
 
-def make_on_startup_handler(bot: Bot, webhook_url: str) -> EventHandler:
+def make_on_startup_handler(bot: Bot, app: App, webhook_url: str) -> EventHandler:
     async def on_startup(dispatcher: Dispatcher):
+        await app.setup()
         await bot.set_webhook(webhook_url, drop_pending_updates=False)
 
     return on_startup
 
 
-def make_on_shutdown_handler(bot: Bot) -> EventHandler:
+def make_on_shutdown_handler(bot: Bot, app: App) -> EventHandler:
     async def on_shutdown(dispatcher: Dispatcher):
         await bot.delete_webhook()
+        await app.cleanup()
 
     return on_shutdown
