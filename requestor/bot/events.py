ad52fe0c03dd3ec5aacda3d5b59b81c2638613fb
Фельдман Эмилий Олегович <eofeldman@0000nba0a4zmd6m.msk.mts.ru>
2022-11-06 20:29:49 +0500

    fixed asyncpg error

--- requestor/bot/events.py
+++ requestor/bot/events.py
@@ -2,15 +2,22 @@ import typing as tp
 
 from aiogram import Bot, Dispatcher
 
-from requestor.services import App
+from requestor.services import App, make_db_service
 
+from ..settings import ServiceConfig
 from .commands import BotCommands
 
 EventHandler = tp.Callable[[Dispatcher], tp.Coroutine[tp.Any, tp.Any, tp.Any]]
 
 
-def make_on_startup_handler(bot: Bot, app: App, webhook_url: tp.Optional[str]) -> EventHandler:
+def make_on_startup_handler(
+    bot: Bot, app: App, webhook_url: tp.Optional[str], config: ServiceConfig
+) -> EventHandler:
     async def on_startup(dispatcher: Dispatcher) -> None:
+        # Do initialization again because of asyncio/asyncpg error
+        # https://github.com/sqlalchemy/sqlalchemy/issues/6409
+        app.db_service = make_db_service(config)
+
         await app.setup()
         await bot.set_my_commands(commands=BotCommands.get_bot_commands())
         if webhook_url is not None:
